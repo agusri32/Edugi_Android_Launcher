c9f6568b378530341902da5b5e579c953ae65cad
Agus Sumarna <sumarna.agus@gmail.com>
2019-10-12 10:38:17 +0700

    simple webview

--- app/src/main/java/xyz/imtiyaz/weblauncher/MainActivity.java
+++ app/src/main/java/xyz/imtiyaz/weblauncher/MainActivity.java
@@ -2,68 +2,19 @@ package xyz.imtiyaz.weblauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView websiteku;
-
-    //URL web, yang akan kita gunakan pada Webview
-    private String URL = "https://webedukasi.000webhostapp.com";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        websiteku = findViewById(R.id.my_web);
-        settings();
-        load_website();
-    }
 
-    //Method Ini Digunakan sebagai Setelan/Pengaturan Web
-    private void settings(){
-        WebSettings web_settings = websiteku.getSettings();
-        web_settings.setJavaScriptEnabled(true);
-        web_settings.setAllowContentAccess(true);
-        web_settings.setUseWideViewPort(true);
-        web_settings.setLoadsImagesAutomatically(true);
-        web_settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        web_settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
-        web_settings.setEnableSmoothTransition(true);
-        web_settings.setDomStorageEnabled(true);
-    }
-
-    //Untuk Mengatur, dan menampilan Halaman Web pada Aplikasi
-    private void load_website(){
-        if(Build.VERSION.SDK_INT >= 19){
-            websiteku.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        }else{
-            websiteku.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-
-        websiteku.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.toString());
-                return true;
-            }
-        });
-
-        websiteku.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        websiteku.loadUrl(URL);
-    }
-
-    public void onBackPressed() {
-        if(websiteku.canGoBack()){
-            websiteku.goBack();
-        }else{
-            super.onBackPressed();
-        }
+        WebView web = (WebView) findViewById(R.id.web_view);
+        web.loadUrl("https://webedukasi.000webhostapp.com");
+        web.setWebViewClient(new WebViewClient());
     }
 }

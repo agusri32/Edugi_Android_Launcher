55c44f8fad218fe7b1f2e3218ff66ee507edf0c4
Agus Sumarna <sumarna.agus@gmail.com>
2019-10-14 10:44:37 +0700

    javascript

--- app/src/main/java/xyz/imtiyaz/weblauncher/MainActivity.java
+++ app/src/main/java/xyz/imtiyaz/weblauncher/MainActivity.java
@@ -3,6 +3,7 @@ package xyz.imtiyaz.weblauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -13,8 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView web = (WebView) findViewById(R.id.web_view);
-        web.loadUrl("https://webedukasi.000webhostapp.com");
-        web.setWebViewClient(new WebViewClient());
+        WebView myWeb = (WebView) findViewById(R.id.web_view);
+        myWeb.loadUrl("https://webedukasi.000webhostapp.com");
+        myWeb.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = myWeb.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(false);
     }
 }
